7ae4944970200b34621a766c13db8ae8dc390cff
FelipeFavato <felipelopesfavato@gmail.com>
2023-07-04 11:11:30 -0300

    Finalizando exercicios do dia

---
+++ exercicios/exercicio_2.py
@@ -0,0 +1,26 @@
+# Utilize o módulo random da linguagem Python para gerar um array com 100 int.
+# Cada um deve ser a média de n números gerados aleatoriamente.
+# Qual é a ordem de complexidade de tempo e de espaço deste programa?
+
+""" Mesmo que, para o exemplo dado, o valor de `n` seja muito menor que o
+valor da constante `100`, para valores de `n` grandes o valor da constante
+se torna desprezível. Esse problema, então, é `O(n)`.
+Pelo mesmo motivo, a complexidade de espaço é constante, ou seja, `O(1)`"""
+
+import random
+
+
+def random_averages(n):
+    list_of_averages = []
+
+    for _ in range(100):
+        average = 0
+        for _ in range(n):
+            average += random.randrange(1, n)
+        average = average/n
+        list_of_averages.append(average)
+
+    return list_of_averages
+
+
+print(random_averages(100))
