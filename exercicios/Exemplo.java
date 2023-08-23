4bd744ea50537f383f9dd534c52e61c382a06fe2
FelipeFavato <felipelopesfavato@gmail.com>
2023-08-23 13:07:49 -0300

    escopo de variaveis

---
+++ exercicios/Exemplo.java
@@ -0,0 +1,10 @@
+public class Exemplo {
+  // static String name = new String("Felipe");
+  static String name = "Felipe";
+
+  public static void main(String[] args) {
+    System.out.println("Hello world");
+    System.out.println(name);
+
+  }
+}
---
+++ exercicios/Teste.java
@@ -0,0 +1,34 @@
+// class Teste{
+
+//   // Variável estatica -> inteiro id = 1, pode ser acessada
+//   // em qualquer lugar
+//   static int id = 1;
+
+//   // Metodo estatico
+//   // public => Metodo publico, pode ser acessado fora da classe,
+//   // ou seja, qualquer outra classe pode acessar
+//   // static => Pertence a classe, nao ao objeto
+//   // void => Indica que esse metodo nao retorna nada
+//   // String[] args => Pede que os argumentos passados a esse
+//   // metodo seja um array de strings
+//   public static void main(String[] args){
+//     // Imprime o valor atual
+//     System.out.println(id);
+//     // Soma 1 ao valor atual
+//     id = id + 1;
+//   }
+// }
+
+
+
+// class Teste {
+//     static int i = 3;
+//     public static void main(String[] a) {
+//         for (new Teste().i = 10; new Teste().i < 100;
+//              new Teste().i++) {
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+
